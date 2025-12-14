b8411653e7a5e0787cd29e41bd225864298ce3f2
sangeetadey99 <sdey6268@gmail.com>
2025-12-14 16:47:34 +0530

    Reusable Component and Pass Props

---
+++ module-3/reactday2/reusable-component/src/App.jsx
@@ -0,0 +1,15 @@
+import MessageCard from "./MessageCard"
+
+function App() {
+ 
+  return (
+    <>
+     <MessageCard title="React -" message="React is a js library."/>
+     <MessageCard title="JavaScript -"  message="JavaScript is a programing language."/>
+     <MessageCard title="CSS -"  message="css is used in styling the webpage."/>
+     <MessageCard title="HTML -"  message="html is a markup language."/>
+    </>
+  )
+}
+
+export default App
---
+++ module-3/reactday2/reusable-component/src/MessageCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MessageCard = ({title, message}) => {
+  return (
+    <>
+    <h1>{title} {message}</h1>
+    </>
+  )
+}
+
+export default MessageCard
